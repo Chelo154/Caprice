6ccc9d1bd26a216db3f26c1d6c19af1e9c0d35e1
Tu Nombre <you@example.com>
2020-02-16 22:50:22 -0300

    descripcion

--- Caprice/resources/views/index.blade.php
+++ Caprice/resources/views/index.blade.php
@@ -5,13 +5,13 @@
 
 @section('content')
 
- 
+
 
 <div class="container fondo-base-1" >
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <hr class="linea-gris">             
+                <hr class="linea-gris">
             <div class="col-sm-12" >
                 <img src="https://i.ibb.co/T4zSm9P/Caprice-Logo.jpg" class="img-fluid logo-caprice-1 " alt="">
             </div>
@@ -22,24 +22,24 @@
             <div class="col-sm-12">
             <nav class="navbar navbar-expand navbar-dark bg-dark justify-content-center ">
                 <div class="nav navbar-nav">
-                    <a class="nav-item nav-link active " href="{{url('/inicio')}}">                        
-                        <h4 class="letra-caprice text center">Inicio</h4>                         
-                <a class="nav-item nav-link" href="{{url('/nosotros')}}">                        
-                        <h4 class="letra-caprice text center">Quienes Somos</h4>                                                
+                    <a class="nav-item nav-link active " href="{{url('/inicio')}}">
+                        <h4 class="letra-caprice text center">Inicio</h4>
+                <a class="nav-item nav-link" href="{{url('/nosotros')}}">
+                        <h4 class="letra-caprice text center">Quienes Somos</h4>
                 </a>
             <a class="nav-item nav-link" href="{{url('/foodandbeer')}}">
-                    <h4 class="letra-caprice text center">Food and Beer</h4>                        
+                    <h4 class="letra-caprice text center">Food and Beer</h4>
                 </a>
             <a class="nav-item nav-link" href="{{url('/contactanos')}}">
-                    <h4 class="letra-caprice text center">Contáctanos</h4>                       
+                    <h4 class="letra-caprice text center">Contáctanos</h4>
             </a>
                 </div>
-            </nav>    
+            </nav>
             </div>
-            <!-- Inicio del contenido-->                 
+            <!-- Inicio del contenido-->
             <div class="col-md-6"><!--Intro Carousel-->
                 <h2 class="letra-caprice-blanca "> Para vos</h2>
-                <hr class="linea-naranja"/>            
+                <hr class="linea-naranja"/>
             </div><!--/Intro Carousel-->
             <div class="col-md-12"><!--ColumnaCarousel -->
                 <div id="carouselId" class="carousel slide" data-ride="carousel">
@@ -51,7 +51,7 @@
                     </ol>
                     <div class="carousel-inner" role="listbox">
                         <div class="carousel-item active">
-                            <img src="https://i.ibb.co/qMZ3WTC/Carousel-Caprice1.jpg" alt="First slide" class="img-fluid imagen-carousel-1">                                                     
+                            <img src="https://i.ibb.co/qMZ3WTC/Carousel-Caprice1.jpg" alt="First slide" class="img-fluid imagen-carousel-1">
                         </div>
                         <div class="carousel-item">
                             <img src="https://i.ibb.co/T28XtbV/Carousel-Caprice2.jpg" alt="Second slide" class="img-fluid imagen-carousel-1">
@@ -62,7 +62,7 @@
                         <div class="carousel-item">
                             <img src="https://i.ibb.co/vXLTDvL/Carousel-Caprice4.jpg" alt="Fourth slide" class="img-fluid imagen-carousel-1">
                         </div>
-                        
+
                     </div>
                     <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -73,22 +73,24 @@
                         <span class="sr-only">Siguiente</span>
                     </a>
                 </div>
-               
-            </div>                   
+
+            </div>
         </div><!--/ColumnaCarousel -->
         <div class="col-md-6"><!--Intro Presentación-->
             <h2 class="letra-caprice-blanca">Bienvenido a Caprice </h2>
-            <hr class="linea-naranja"/>            
+            <hr class="linea-naranja"/>
         </div><!--/Intro Presentacion-->
         <div class="col-sm-12">
-            <h4 class="letra-info-blanca">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae cupiditate nobis, libero quis molestias a neque quaerat tempore incidunt saepe assumenda enim beatae in sequi totam, adipisci repellat nihil harum omnis ratione soluta fugit! Nulla eos perferendis error modi unde voluptates debitis. Nulla id laudantium ducimus magni excepturi, numquam eveniet culpa, placeat perspiciatis et natus ut, animi atque repellendus voluptas expedita facere. Soluta, minus? Perferendis voluptatum cumque corporis cum sapiente iusto velit natus, voluptates ipsam dolorum, non dolorem sunt optio architecto consequatur commodi iste dolores! Modi esse dolorum autem quasi.   
+            <h4 class="letra-info-blanca" >
+                Caprice, es una bar nueva ubicada en Barrio Sur de San Miguel de Tucumán. especificamente en Chacabuco 437<br>
+                Vení y disfruta todos los días de la semana con buenas promos y  nuestro happy hour en pintas y comida de calidad!
+                Te esperamos para que pases un agradeble momento o celebres tu cumple con amigos , familia , pareja!
             </h4>
         </div>
-        <!--/Contenido de la Pagina--> 
+        <!--/Contenido de la Pagina-->
     </div>
 </div>
 <link href="{!! asset('css/estilosbase.css') !!}" rel="stylesheet">
 <link href="{!! asset('css/fonts.css') !!}" rel="stylesheet">
-    
+
 @endsection
