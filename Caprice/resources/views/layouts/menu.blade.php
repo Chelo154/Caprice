8be06384c1f966c069676000fc0f5df20263c6f0
Tu Nombre <you@example.com>
2020-02-20 23:23:03 -0300

    Agregue links a Lista de Producto y Nuevo Producto

--- Caprice/resources/views/layouts/menu.blade.php
+++ Caprice/resources/views/layouts/menu.blade.php
@@ -17,12 +17,10 @@
     <div class="bg-dark border-right" id="sidebar-wrapper">
       <div class="sidebar-heading letra-caprice">Caprice-Empleados </div>
       <div class="list-group list-group-flush ">
-        <a href="#" class="list-group-item list-group-item-action bg-dark letra-negocio-blanca">Dashboard</a>
-        <a href="#" class="list-group-item list-group-item-action bg-dark letra-negocio-blanca">Shortcuts</a>
-        <a href="#" class="list-group-item list-group-item-action bg-dark letra-negocio-blanca">Overview</a>
-        <a href="#" class="list-group-item list-group-item-action bg-dark letra-negocio-blanca">Events</a>
-        <a href="#" class="list-group-item list-group-item-action bg-dark letra-negocio-blanca">Profile</a>
-        <a href="#" class="list-group-item list-group-item-action bg-dark letra-negocio-blanca">Status</a>
+        <a href="{{url ('/products')}}" class="list-group-item list-group-item-action bg-dark letra-negocio-blanca">Lista de Productos</a>
+        <a href="{{url('/products/create')}}" class="list-group-item list-group-item-action bg-dark letra-negocio-blanca">Nuevo Producto</a>
+        <a href="#" class="list-group-item list-group-item-action bg-dark letra-negocio-blanca">Nueva Comanda</a>
+        <a href="#" class="list-group-item list-group-item-action bg-dark letra-negocio-blanca">Cerrar Sesión</a>
       </div>
     </div>
     <!-- /#sidebar-wrapper -->
@@ -37,27 +35,6 @@
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse " id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
-        </div>
       </nav>
 
       <div class="container-fluid fondo-madera">
@@ -69,12 +46,6 @@
   </div>
   <!-- /#wrapper -->
 
-  <!-- Menu Toggle Script -->
-
-
-
-
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 
@@ -85,6 +56,7 @@
     <link href="{!! asset('css/simple-sidebar.css') !!}" rel="stylesheet">
     <link href="{!! asset('css/layoutemployee.css') !!}" rel="stylesheet">
     <link href="{!! asset('css/fonts.css') !!}" rel="stylesheet">
+    <!-- Menu Toggle Script -->
     <script>
         $("#menu-toggle").click(function(e) {
           e.preventDefault();
